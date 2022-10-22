1c425724b6919b13cb57bfa1e0bea34967f523ca
ForgottenGod <91021642+forggod@users.noreply.github.com>
2022-10-22 14:57:42 +0300

    Update main.py

--- main.py
+++ main.py
@@ -14,14 +14,6 @@ sql.execute("""CREATE TABLE IF NOT EXISTS `types` (
   `type` varchar(1024) NOT NULL UNIQUE
 )""")
 # --
-# -- Структура таблицы `sales`
-# --
-sql.execute("""CREATE TABLE IF NOT EXISTS `sales` (
-  `id` integer NOT NULL PRIMARY KEY AUTOINCREMENT,
-  `customer` integer(11) NOT NULL,
-  `status` boolean(1) NOT NULL DEFAULT 0
-)""")
-# --
 # -- Структура таблицы `supliers`
 # --
 sql.execute("""CREATE TABLE IF NOT EXISTS `supliers` (
@@ -30,6 +22,14 @@ sql.execute("""CREATE TABLE IF NOT EXISTS `supliers` (
   `location` varchar(1024) NOT NULL,
   `type_id` integer(11) NOT NULL,
    FOREIGN KEY ('type_id') REFERENCES 'types' ('id') ON DELETE CASCADE ON UPDATE CASCADE
+)""")
+# --
+# -- Структура таблицы `sales`
+# --
+sql.execute("""CREATE TABLE IF NOT EXISTS `sales` (
+  `id` integer NOT NULL PRIMARY KEY AUTOINCREMENT,
+  `customer` integer(11) NOT NULL,
+  `status` boolean(1) NOT NULL DEFAULT 0
 )""")
 # --
 # -- Структура таблицы `customer_list`
@@ -44,7 +44,15 @@ sql.execute("""CREATE TABLE IF NOT EXISTS `customer_list` (
     );""")
 # endregion
 
-# sql.execute("SELECT name FROM sqlite_master WHERE type='table';")
+
+sql.execute("SELECT type, company, location FROM types, supliers WHERE types.id = supliers.type_id order by type")
+tables = sql.fetchall()
+for line in tables:
+    print(*line)
+print()
+
+# sql.execute("SELECT customer, status, type, count, company, location FROM types, supliers, sales, "
+#             "customer_list WHERE sales.id = customer_list.customer_id and customer_list.type_id = types.id;")
 # tables = sql.fetchall()
 # for line in tables:
 #     print(line)
@@ -57,8 +65,14 @@ sql.execute("""CREATE TABLE IF NOT EXISTS `customer_list` (
 #                                                                        ('USWOOD', 'США', '2'), ('USWOOD', 'США', '1'), ('SIBWD', 'Россия', '7'),
 #                                                                        ('SIBWD', 'Россия', '2')""")
 
-sql.execute("""""")
+# sql.execute("""INSERT INTO sales(customer, status) VALUES (('IP_1'),('0')), (('IP_2'),('0')), (('IP_3'),('0')), (('IP_4'),('0')), (('IP_5'),('0')),
+#                                                           (('IP_6'),('0')), (('IP_7'),('0')), (('IP_8'),('0')), (('IP_9'),('0')), (('IP_10'),('0'))""")
 
-db.commit()
+# sql.execute("""INSERT INTO customer_list(customer_id, type_id, count) VALUES (('1'), ('5'), ('1000')), (('2'), ('1'), ('10000')), (('3'), ('2'), ('3000')),
+#                                                                              (('4'), ('7'), ('5000')), (('5'), ('3'), ('8000')), (('6'), ('5'), ('4000')),
+#                                                                              (('7'), ('1'), ('500')), (('8'), ('3'), ('1000')), (('9'), ('5'), ('1000')),
+#                                                                              (('10'), ('2'), ('1000'))""")
+
+# db.commit()
 
 sql.close()
